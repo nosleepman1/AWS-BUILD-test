34355410d5060108c48f0721d0e83d98ea11c700
nosleepman1 <abdallahdiouf.dev@gmail.com>
2026-03-27 03:29:19 +0000

    feat: add `sayHello` and `sayGoodbye` functions along with their unit tests.

---
+++ myApp.py
@@ -0,0 +1,10 @@
+def sayHello(name):
+    return "Hello " + name
+
+def sayGoodbye(name):
+    return "Goodbye " + name
+
+
+if __name__ == "__main__":
+    print(sayHello("AWS"))
+    print(sayGoodbye("AWS"))    
---
+++ test.py
@@ -0,0 +1,16 @@
+import unittest
+from myApp import sayGoodbye, sayGoodbye
+
+
+
+class TestApp(unittest.TestCase):
+    def test_sayHello(self):
+        self.assertEqual(sayHello("AWS"), "Hello AWS")
+    
+    def test_sayGoodbye(self):
+        self.assertEqual(sayGoodbye("AWS"), "Goodbye AWS")
+
+
+
+if __name__ == "__main__":
+    unittest.main()
